c88d5fbec183578b88583f6cbe68273cc724ab49
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-12-04 11:05:46 -0800

    rm: port activities

--- src/js/models/port.js
+++ src/js/models/port.js
@@ -3,13 +3,6 @@ define(function (require) {
   var Backbone = require('backbone');
 
   var Port = Backbone.Model.extend({
-    initialize: function () {
-      this._activities = [];
-    },
-
-    addActivity: function (act) {
-      this._activities.push(act);
-    }
   });
 
   return Port;
