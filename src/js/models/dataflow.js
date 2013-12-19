c17799ab7be4060f1c7ef8c6d6c633f8e57c7bc5
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-12-18 16:18:44 -0800

    add: documents of dataflow model

--- src/js/models/dataflow.js
+++ src/js/models/dataflow.js
@@ -5,16 +5,43 @@ define(function (require) {
   var DataLink   = require('models/datalink');
   var DataLinks  = require('collections/datalinks');
 
+  /**
+   * @class Dataflow
+   * @classdesc The Dataflow class acts as the container of processors and
+   *   datalinks. It's also responsible for propagating the signal values
+   *   from the source processors to their descendents.
+   */
   var Dataflow = Backbone.Model.extend({
+    /**
+     * @public
+     * @constructs Dataflow
+     */
     initialize: function () {
       this.processors = new Processors({dataflow: this});
       this.datalinks = new DataLinks();
     },
 
+    /**
+     * Add a processor into the dataflow.
+     * NOTE: the dataflow will rebuild the dependency graph after
+     * calling this method. If you want to add multiple processors
+     * into the dataflow, use addProcessors instead.
+     *
+     * @public
+     * @method
+     * @param {Processor} processor The processor to add
+     */
     addProcessor: function (processor) {
       this.addProcessors([processor]);
     },
 
+    /**
+     * Add multiple processors into the dataflow.
+     *
+     * @public
+     * @method
+     * @param {Array.<Processor>} processors The processors to add.
+     */
     addProcessors: function (processors) {
       _.each(processors, function (p) {
         p.setDataflow(this);
@@ -25,10 +52,51 @@ define(function (require) {
       this.trigger('add:processors', processors);
     },
 
+    /**
+     * Get the processors stored in the dataflow.
+     *
+     * @public
+     * @method
+     * @returns {Processors}
+     */
     getProcessors: function () {
       return this.processors;
     },
 
+    /**
+     * Add a datalink into the dataflow.
+     * NOTE: the dataflow will rebuild the dependency graph after
+     * calling this method. If you want to add multiple datalinks
+     * into the dataflow, use addDataLinks instead.
+     *
+     * @public
+     * @method
+     * @param {DataLink} datalink The datalink to add
+     */
+    addDataLink: function (datalink) {
+      this.addDataLinks([datalink]);
+    },
+
+    /**
+     * Add multiple datalinks into the dataflow.
+     *
+     * @public
+     * @method
+     * @param {Array.<DataLink>} datalink The datalink to add
+     */
+    addDataLinks: function (datalinks) {
+      this.datalinks.add(datalinks);
+      this.buildDependencyGraph();
+      this.trigger('add:datalinks', datalinks);
+    },
+
+    /**
+     * Build the dependency graph from the processors and datalinks.
+     * The dependency graph will later be used for propagating the
+     * values of the source processors to their descendents.
+     *
+     * @private
+     */
     buildDependencyGraph: function () {
       var descendents = this.descendents = {};
       var indegree = this.indegree = {};
@@ -65,13 +133,21 @@ define(function (require) {
       });
     },
 
+    /**
+     * Propagate the values of the source processors to their
+     * descendents.
+     * NOTE: this method is using the topological sorting algorithm
+     * to propagate the values.
+     *
+     * @private
+     */
     updateValues: function () {
       // cache properties for faster access
       var indegree = _.clone(this.indegree);
       var descendents = _.clone(this.descendents);
       var processors = this.processors;
 
-      // for each processor, if it has an indegree of 0, then push it into a queue
+      // for each processor id, if it has an indegree of 0, then push it into a queue
       var queue = [];
       _.each(indegree, function (value, processorId) {
         if (value === 0) {
@@ -80,11 +156,16 @@ define(function (require) {
       });
 
       while (queue.length) {
+        // take the first processor id from the queue.
         var processorId = queue.shift();
         var processor = processors.get(processorId);
 
+        // for each processor taken from the queue, it has an indegree of 0,
+        // therefore, all its input ports are already ready to use. 
         processor.updateOutputPortValues();
 
+        // for each of the descendent processor ids, decrease its indegree by 1,
+        // and push it into the queue if it reaches 0.
         processor.getOutputPorts().each(function (port) {
           var id = port.cid;
           _.each(descendents[id], function (descendentId) {
@@ -95,12 +176,6 @@ define(function (require) {
           });
         });
       }
-    },
-
-    addDataLink: function (datalink) {
-      this.datalinks.add(datalink);
-      this.buildDependencyGraph();
-      this.trigger('add:datalink', datalink);
     }
   });
 
