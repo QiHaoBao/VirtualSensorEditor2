5f46ac74efbcf6c4c09dd49afd291f398f9b79c4
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-12-19 15:28:18 -0800

    add: document of physical sensor

--- src/js/models/physical_sensor.js
+++ src/js/models/physical_sensor.js
@@ -3,24 +3,63 @@ define(function (require) {
   var Backbone = require('backbone');
   var Processor = require('models/processor');
 
+  /**
+   * A PhysicalSensor is a processor which has no input ports and
+   * has exactly one output port named 'out'.
+   * It also has a device id and sensor type.
+   *
+   * @class Physicalsensor
+   */
   var PhysicalSensor = Processor.extend({
+
+    /**
+     * @constructs Physicalsensor
+     */
     initialize: function () {
       Processor.prototype.initialize.call(this);
       this.addOutputPort('out');
     },
 
+    /**
+     * Get the device id of the sensor.
+     *
+     * @public
+     * @method
+     * @returns {String}
+     */
     getDeviceId: function () {
       return this.get('deviceId');
     },
 
+    /**
+     * Get the type of the sensor.
+     *
+     * @public
+     * @method
+     * @returns {String}
+     */
     getType: function () {
       return this.get('type');
     },
 
+    /**
+     * Get the value of the sensor.
+     *
+     * @public
+     * @method
+     * @returns {*}
+     */
     getValue: function () {
       return this.getOutputPortValue('out');
     },
 
+    /**
+     * Set the value of the sensor.
+     *
+     * @public
+     * @method
+     * @param {*} value
+     */
     setValue: function (value) {
       this.setOutputPortValue('out', value);
     }
